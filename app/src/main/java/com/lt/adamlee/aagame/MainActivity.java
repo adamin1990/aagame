9b97bf592ac48441404a31041954cc19a6d511bd
XiaoPengLi <adamin1990@gmail.com>
2015-07-28 22:37:40 +0800

    init project
    init the proj

---
+++ app/src/main/java/com/lt/adamlee/aagame/MainActivity.java
@@ -0,0 +1,91 @@
+package com.lt.adamlee.aagame;
+
+import android.content.Intent;
+import android.graphics.Rect;
+import android.os.Handler;
+import android.support.v4.media.TransportMediator;
+import android.support.v4.view.accessibility.AccessibilityNodeInfoCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ProgressBar;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+    private static int progress;
+    private Handler handler;
+    public boolean isClosed;
+    @Bind(R.id.progressBar1)
+    ProgressBar progressBar;
+
+    private int progressStatus;
+
+    public MainActivity() {
+        this.progressStatus = 0;
+        this.handler = new Handler();
+    }
+
+    static {
+        progress = 0;
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(1);
+        getWindow().setFlags(AccessibilityNodeInfoCompat.ACTION_NEXT_HTML_ELEMENT, AccessibilityNodeInfoCompat.ACTION_NEXT_HTML_ELEMENT);
+        getWindow().setFlags(TransportMediator.FLAG_KEY_MEDIA_NEXT, TransportMediator.FLAG_KEY_MEDIA_NEXT);
+
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        this.isClosed = false;
+        Rect bounds = progressBar.getProgressDrawable().getBounds();
+        this.progressBar.setProgressDrawable(getResources().getDrawable(R.drawable.progress));
+        this.progressBar.getProgressDrawable().setBounds(bounds);
+
+
+    }
+
+
+    public void thread() {
+        new Thread(new Runnable() {
+            public void run() {
+                while (progressStatus < 100) {
+                   progressStatus = doSomeWork();
+                   handler.post(new Runnable() {
+                       public void run() {
+                           progressBar.setProgress(progressStatus);
+                       }
+                   });
+                }
+               handler.post(new Runnable() {
+                    public void run() {
+                        if (!isClosed) {
+                            startActivity(new Intent(MainActivity.this, MainActivity.class));
+                        }
+                        progressBar.setVisibility(4);
+                        finish();
+                    }
+                });
+            }
+
+            private int doSomeWork() {
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                }
+                int access$4 = progress + 1;
+                progress = access$4;
+                return access$4;
+            }
+        }).start();
+    }
+
+
+
+
+}
